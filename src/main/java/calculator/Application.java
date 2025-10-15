3261f45a10d4e46ea94dea4a68b9b84d26318f51
woongjee <jeewoong7753@naver.com>
2025-10-15 16:16:15 +0900

    feat : 전체 프로그램의 흐름을 제어하는 main() 메소드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,9 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        input();
+        decideSeparator(s);
+        System.out.println("결과 : "+ calculate());
     }
 
     private static void input(){
