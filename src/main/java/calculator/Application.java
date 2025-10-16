75f2749ad65b64af0d07c5903b713bc2117fd16c
woongjee <jeewoong7753@naver.com>
2025-10-16 16:25:30 +0900

    feat : customSeparator 메소드에서 입력 형식 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,48 +11,58 @@ public class Application {
         // TODO: 프로그램 구현
         input();
         decideSeparator(s);
-        System.out.println("결과 : "+ calculate());
+        System.out.println("결과 : " + calculate());
     }
 
-    private static void input(){
+    private static void input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
         s = sc.nextLine();
         s = s.replace("\\n", "\n"); //custom 구분자 사용 시 \n가 이스케이프로 사용되는 것을 방지
         //사용자 입력이 잘못되었을 경우
-        if(s.startsWith("//")){
+        if (s.startsWith("//")) {
             int newLineIndex = s.indexOf("\n");
-            if(newLineIndex == -1){
+            if (newLineIndex == -1) {
                 throw new IllegalArgumentException("잘못된 입력 형식");
             }
         }
     }
 
     private static String customSeparator(String str) {
+        if (!str.startsWith("//") && !str.contains("\n")) {
+            throw new IllegalArgumentException("잘못된 입력 형식");
+        }
         String seperator = str.charAt(2) + "";
         s = s.substring(4);
         return seperator;
     }
 
     private static void decideSeparator(String s) {
-        if(!s.startsWith("/")) seperator = "[,:]";
-        else seperator = customSeparator(s);
+        if (!s.startsWith("/")) {
+            seperator = "[,:]";
+        } else {
+            seperator = customSeparator(s);
+        }
     }
 
-    private static int calculate(){
+    private static int calculate() {
         String[] arr = s.split(seperator);
-        for(String token : arr){
-            if(Integer.parseInt(token) <=0) throw new IllegalArgumentException("양수만 입력 가능");
-            try{
+        for (String token : arr) {
+            if (Integer.parseInt(token) <= 0) {
+                throw new IllegalArgumentException("양수만 입력 가능");
+            }
+            try {
                 Integer.parseInt(token);
-            } catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("숫자가 아닌 값이 입력");
             }
         }
-        for(String str : arr) {
+        for (String str : arr) {
             for (char c : str.toCharArray()) {
-                if((!Character.isDigit(c))) continue;
-                result += Integer.parseInt(c+"");
+                if ((!Character.isDigit(c))) {
+                    continue;
+                }
+                result += Integer.parseInt(c + "");
             }
         }
         return result;
